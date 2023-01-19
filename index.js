6670b301385b988971881df3dbeca63b8a2dc21d
Sabine Sun <84514455+SabineSun@users.noreply.github.com>
2023-01-19 15:23:03 +0100

    feat : add workshop2

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const locationController = require('./locations/locations.controller')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express()
 const port = 3000
 
+// Middleware
+
+
+
+// Routes
 
 app.use(locationController)
 
-app.listen(port, () => {
-	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
+app.get('/', (req, res) => {
+	return res.status(200).send({
+		message:"Hello World",
+
+	});
 })
+
+function main(){
+	mongoose.connect(process.env.MONGO_URI).then((success) => {
+		console.log("Connection is successfull");
+	app.listen(port, () => {
+		console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
+	})
+	})
+}
+
+main();
--- locations/locations.controller.js
+++ locations/locations.controller.js
@@ -4,9 +4,13 @@
 const router = require('express').Router()
 const locationsService = require('./locations.service')
 
-router.get('/locations', (req, res) => {
+/*router.get('/locations', (req, res) => {
 	return res.status(200).send({locations: []})
-})
+})*/
 
+router.route('/locations')
+	.get(async (req, res) => {
+		return res.status(200).send({locations: await locationsService.findAll()});
+	})
 
 module.exports = router
